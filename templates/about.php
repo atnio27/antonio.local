7ed3ba57e3472c4ae4172f06d0bb7a1ffbfdf115
Antonio Esclapez <titoway33@gmail.com>
2024-10-02 11:22:40 +0200

    Primer Commit

---
+++ templates/about.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/views/about.view.php';
+
+require_once __DIR__ . '/../src/entity/imagen.class.php';
+
+$imagenesClientes[] = new Imagen('client1.jpg', 'MISS BELLA', 'I');
